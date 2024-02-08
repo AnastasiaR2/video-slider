544d865b594a38a88bdac8e55ae8341bb13606c2
AnastasiaR2 <anastasiia.romaniuk007@gmail.com>
2024-02-08 16:58:03 +0200

    Added two sliders for images and videos

--- src/js/components/slider/slider.js
+++ src/js/components/slider/slider.js
@@ -1,1 +1,45 @@
-import { ImageSlider, VideoSlider } from '../slider-settings.js';
+import { createElement } from '../../helpers/create-element.helper.js';
+import { createSliderElement } from './helpers/create-slider-element.helper.js';
+import { ImageSlider, VideoSlider } from './slider-settings.js';
+
+const imageSlider = new ImageSlider('.image-slider');
+imageSlider.init();
+
+const videoSlider = new VideoSlider('.video-slider');
+videoSlider.init();
+
+const createSlider = (videoData) => {
+  const { pictures, embed } = videoData;
+  const imageSlides = [];
+  for (let i = 0; i < 8; i++) {
+    imageSlides.push(`<img src="${pictures.base_link}">`);
+  }
+
+  const videoSlides = [];
+  for (let i = 0; i < 8; i++) {
+    videoSlides.push(`${embed.html}`);
+  }
+
+  const imageSliderElement = createSliderElement({
+    selector: '.image-slider',
+    slides: imageSlides,
+    hasNavigation: true,
+  });
+
+  const videoSliderElement = createSliderElement({
+    selector: '.video-slider',
+    slides: videoSlides,
+    hasPagination: true,
+  });
+
+  const sliderElement = createElement({
+    tagName: 'main',
+  });
+
+  sliderElement.appendChild(imageSliderElement);
+  sliderElement.appendChild(videoSliderElement);
+
+  return sliderElement;
+};
+
+export { createSlider };
--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,4 @@
+import { createSlider } from './components/slider/slider.js';
 import { fetchVideo } from './services/api.js';
 
 const rootElement = document.querySelector('#root');
@@ -5,8 +6,9 @@ const rootElement = document.querySelector('#root');
 const renderApp = async () => {
   try {
     rootElement.innerText = 'Loading...';
-    const video = await fetchVideo(824804225);
-    rootElement.innerHTML = `<img src="${video.pictures.base_link}">`;
+    const videoData = await fetchVideo(824804225);
+    const sliderElement = createSlider(videoData);
+    rootElement.appendChild(sliderElement);
   } catch (error) {
     rootElement.innerText = 'Failed to load app';
     throw error;
