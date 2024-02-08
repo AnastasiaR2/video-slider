3be4e660c9bf4c9ca6bb56f20cf3cd58cb8e396a
AnastasiaR2 <anastasiia.romaniuk007@gmail.com>
2024-02-08 15:57:45 +0200

    Created classes for slider settings

---
+++ src/js/components/slider-settings.js
@@ -0,0 +1,47 @@
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+
+class BaseSlider {
+  constructor(selector, options) {
+    this.selector = selector;
+    this.options = options;
+  }
+
+  init() {
+    return new Swiper(this.selector, {
+      loop: true,
+      simulateTouch: false,
+      ...this.options,
+    });
+  }
+}
+
+class ImageSlider extends BaseSlider {
+  constructor(selector) {
+    super(selector, {
+      modules: [Navigation],
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
+  }
+}
+
+class VideoSlider extends BaseSlider {
+  constructor(selector) {
+    super(selector, {
+      modules: [Pagination],
+      pagination: {
+        clickable: true,
+        el: '.swiper-pagination',
+      },
+    });
+  }
+}
+
+export { ImageSlider, VideoSlider };
